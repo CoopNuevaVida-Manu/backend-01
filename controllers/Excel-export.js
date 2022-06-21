9d5bd53b971a695c658304de7bf8eb55264af3f9
CoopNuevaVida-Manu <mvilleda@coopnuevavida.com>
2022-06-21 15:31:45 -0600

    Actualizacion y reduccion de consultas, cambio a views exitoso

--- controllers/Excel-export.js
+++ controllers/Excel-export.js
@@ -3,47 +3,20 @@ const conexion = require('../DB/db');
 const XLSX = require('xlsx')
 
 const exportTransaccionesSinComprobante = (req, resp = response)=>{
-    conexion.query(`SELECT  tsc.codigo_afiliado as "Codigo de Afiliado", 
-                            tsc.cuenta_afectada as "Cuenta afectada", 
-                            fl.filial as "Filial Pertenece",
-                            tsc.id_cliente as "Identidad Cliente",
-                            UPPER(concat(na.nombre,' ',na.apellido)) as "Nombre",
-                            o.origen_fondos as "Origen de fondos",
-                            tr.transaccion as "Tipo de transaccion",
-                            tsc.fecha_operacion as "Fecha de Operacion", 
-                            tsc.monto_transaccion as "Monto de transaccion",
-                            f.filial as "Filial Operacio",
-                            cb.colaborador_usuario as "Colaborador Usuario",
-                            tsc.observaciones as "Observaciones"
-                    FROM public.transacciones_sin_comprobantes AS tsc
-                    inner join origen_fondos as o on o.id_origen_fondos = tsc.id_origen_fondos
-                    inner join transaccion as tr on tr.id_transaccion = tsc.id_transaccion
-                    inner join filial as f on f.id_filial = tsc.id_filial_realizo_transaccion
-                    inner join filial as fl on fl.id_filial = tsc.id_filial_ac
-                    inner join colaborador as cb on cb.id_colaborador = tsc.id_colaborador
-                    inner join no_afiliado as na on tsc.id_cliente = na.identidad
-
-                    UNION ALL
-
-                    SELECT  tsc.codigo_afiliado as "Codigo de Afiliado", 
-                            tsc.cuenta_afectada as "Cuenta afectada",
-                            fl.filial as "Filial Pertenece",
-                            tsc.id_cliente as "Identidad Cliente",
-                            UPPER(af."OUTAFF_NAME") as "Nombre",
-                            o.origen_fondos as "Origen de fondos",
-                            tr.transaccion as "Tipo de transaccion",
-                            tsc.fecha_operacion as "Fecha de Operacion", 
-                            tsc.monto_transaccion as "Monto de transaccion",
-                            f.filial as "Filial Operacio",
-                            cb.colaborador_usuario as "Colaborador Usuario",
-                            tsc.observaciones as "Observaciones"
-                    FROM public.transacciones_sin_comprobantes AS tsc
-                    inner join origen_fondos as o on o.id_origen_fondos = tsc.id_origen_fondos
-                    inner join transaccion as tr on tr.id_transaccion = tsc.id_transaccion
-                    inner join filial as f on f.id_filial = tsc.id_filial_realizo_transaccion
-                    inner join filial as fl on fl.id_filial = tsc.id_filial_ac
-                    inner join colaborador as cb on cb.id_colaborador = tsc.id_colaborador
-                    inner join public."Afiliados" AS af ON af."OUTAFF_ID" = tsc.id_cliente`, (err, res)=>{
+    conexion.query(`SELECT  codigo_afiliado as "Codigo de Afiliado", 
+                            cuenta_afectada as "Cuenta afectada", 
+                            filialac as "Filial Pertenece", 
+                            id_cliente as "Identidad Cliente", 
+                            UPPER(name) as "Nombre Completo", 
+                            origen_fondos as "Origen de fondos", 
+                            transaccion as "Tipo de transaccion", 
+                            fecha_operacion as "Fecha de Operacion", 
+                            monto_transaccion as "Monto de transaccion", 
+                            filial as "Filial Operacion", 
+                            colaborador_usuario as "Colaborador Usuario", 
+                            observaciones as "Observaciones"
+                    FROM public.rte
+                    ORDER BY fecha_operacion DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -91,7 +64,8 @@ const exporDiligenciaNoAfiliados = (req, resp = response)=>{
                     INNER JOIN razon_operacion AS rp ON rp.id_razon_operacion = DD.id_razon_operacion
                     INNER JOIN transaccion AS ts ON ts.id_transaccion = DD.id_transaccion
                     INNER JOIN colaborador AS cb ON cb.id_colaborador = DD.id_cajero_operacion
-                    INNER JOIN no_afiliado AS na ON na.identidad = DD.id_no_afiliado`, (err, res)=>{
+                    INNER JOIN no_afiliado AS na ON na.identidad = DD.id_no_afiliado
+                    ORDER BY DD.fecha_operacion DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -119,51 +93,20 @@ const exporDiligenciaNoAfiliados = (req, resp = response)=>{
 }
 
 const exporChequesTerceros = (req, resp = response)=>{
-    conexion.query(`SELECT  ct.codigo_de_afiliado AS "Codigo de Afiliado", 
-                            f.filial AS "Filial", 
-                            ct.n_cheque AS "N° de Cheque", 
-                            ct.fecha_emision AS "Fecha de emision", 
-                            UPPER(af."OUTAFF_NAME") AS "Nombre",
-                            ct.id_persona AS "Identidad",
-                            ae.afiliado_estado AS "Estado",
-                            pt.parentesco AS "Parentesco",
-                            ct.monto AS "Monto", 
-                            o.origen_fondos AS "Origen de Fondos",
-                            d.destino AS "Destino",
-                            UPPER(cb.colaborador_nombre) AS "Nombre Colaborador",
-                            ct.observaciones AS "Observaciones"
-                    FROM public.cheques_terceros AS ct
-                    INNER JOIN filial AS f ON f.id_filial = ct.id_filial
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ct.id_colaborador
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ct.id_parentesco
-                    INNER JOIN origen_fondos AS o ON o.id_origen_fondos = ct.id_origen_fondos
-                    INNER JOIN destino AS d ON d.id_destino = ct.id_destino
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ct.id_afililiado_estado
-                    INNER JOIN "Afiliados" AS af ON af."OUTAFF_ID" = ct.id_persona
-
-                    UNION ALL
-
-                    SELECT  ct.codigo_de_afiliado AS "Codigo de Afiliado", 
-                            f.filial AS "Filial", 
-                            ct.n_cheque AS "N° de Cheque", 
-                            ct.fecha_emision AS "Fecha de emision", 
-                            UPPER(concat(na.nombre, ' ', na.apellido)) AS "Nombre",
-                            ct.id_persona AS "Identidad",
-                            ae.afiliado_estado AS "Estado",
-                            pt.parentesco AS "Parentesco",
-                            ct.monto AS "Monto",
-                            o.origen_fondos AS "Origen de Fondos",
-                            d.destino AS "Destino",
-                            UPPER(cb.colaborador_nombre) AS "Nombre Colaborador",
-                            ct.observaciones AS "Observaciones"
-                    FROM public.cheques_terceros AS ct
-                    INNER JOIN filial AS f ON f.id_filial = ct.id_filial
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ct.id_colaborador
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ct.id_parentesco
-                    INNER JOIN origen_fondos AS o ON o.id_origen_fondos = ct.id_origen_fondos
-                    INNER JOIN destino AS d ON d.id_destino = ct.id_destino
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ct.id_afililiado_estado
-                    INNER JOIN no_afiliado AS na ON na.identidad = ct.id_persona`, (err, res)=>{
+    conexion.query(`SELECT  codigo_de_afiliado AS "Codigo de Afiliado", 
+                        filial AS "Filial",
+                        n_cheque AS "N° de Cheque", 
+                        fecha_emision AS "Fecha de emision",
+                        UPPER(name) AS "Nombre",
+                        id_persona AS "Identidad",
+                        afiliado_estado AS "Estado",
+                        parentesco AS "Parentesco",
+                        monto AS "Monto",
+                        origen_fondos AS "Origen de Fondos",
+                        destino AS "Destino",
+                        UPPER(colaborador_nombre) AS "Nombre Colaborador",
+                        observaciones AS "Observaciones"
+                    FROM public.ct;`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -191,51 +134,24 @@ const exporChequesTerceros = (req, resp = response)=>{
 }
 
 const exporFirmasTerceros = (req, resp = response)=>{
-    conexion.query(`SELECT  UPPER(af."OUTAFF_NAME") AS "Nombre",
-                            ft.id_cliente AS "Identidad", 
-                            ae.afiliado_estado AS "Estado",
-                            pt.parentesco AS "Parentesco",
-                            frm.firma AS "Firma",
-                            ft.codigo_afiliado AS "Codigo de Afiliado", 
-                            ft.cuenta_afiliado AS "Cuenta Afectada", 
-                            f.filial AS "Filial Pertenece Afiliado", 
-                            ft.apertura_actualizacion AS "Fecha de Operacion", 
-                            UPPER(cb.colaborador_nombre) AS "Colaborador Nombre",
-                            ft.observaciones AS "Observaciones"
-                    FROM public.firmas_autorizadas_terceros AS ft 
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ft.id_afiliado_estado
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ft.id_parentesco
-                    INNER JOIN firma AS frm ON frm.id_firma = ft.id_firma
-                    INNER JOIN filial AS f ON f.id_filial = ft.id_filial_pertenece
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ft.id_colaborador
-                    INNER JOIN "Afiliados" AS af ON af."OUTAFF_ID" = ft.id_cliente
-
-                    UNION ALL
-
-                    SELECT  UPPER(concat(na.nombre, ' ', na.apellido)) AS "Nombre",
-                            ft.id_cliente AS "Identidad",
-                            ae.afiliado_estado AS "Estado",
-                            pt.parentesco AS "Parentesco",
-                            frm.firma AS "Firma",
-                            ft.codigo_afiliado AS "Codigo de Afiliado", 
-                            ft.cuenta_afiliado AS "Cuenta Afectada", 
-                            f.filial AS "Filial Pertenece Afiliado", 
-                            ft.apertura_actualizacion AS "Fecha de Operacion", 
-                            cb.colaborador_nombre AS "Colaborador Nombre",
-                            ft.observaciones AS "Observaciones"
-                    FROM public.firmas_autorizadas_terceros AS ft 
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ft.id_afiliado_estado
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ft.id_parentesco
-                    INNER JOIN firma AS frm ON frm.id_firma = ft.id_firma
-                    INNER JOIN filial AS f ON f.id_filial = ft.id_filial_pertenece
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ft.id_colaborador
-                    INNER JOIN no_afiliado AS na ON na.identidad = ft.id_cliente`, (err, res)=>{
+    conexion.query(`SELECT  UPPER(name) AS "Nombre",
+                        id_cliente AS "Identidad",
+                        afiliado_estado AS "Estado",
+                        parentesco AS "Parentesco",
+                        firma AS "Firma",
+                        codigo_afiliado AS "Codigo de Afiliado",
+                        cuenta_afiliado AS "Cuenta Afectada",
+                        filial "Filial Pertenece Afiliado",
+                        apertura_actualizacion AS "Fecha de Operacion",
+                        UPPER(colaborador_nombre) AS "Colaborador Nombre",
+                        observaciones AS "Observaciones"
+                    FROM public.ft
+                    ORDER BY apertura_actualizacion DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
             })
         }
-        console.log(res.rows)
         const convertJsonToExcel = () => {
 
             const workSheet = XLSX.utils.json_to_sheet(res.rows);
--- controllers/cumplimiento.js
+++ controllers/cumplimiento.js
@@ -3,49 +3,8 @@ const { response } = require('express');
 const conexion = require('../DB/db');
 
 const getTransaccionesSinComprobante = (req, resp = response)=>{
-    conexion.query(`SELECT tsc.id_transaccion_sc, 
-                        tsc.codigo_afiliado, 
-                        tsc.cuenta_afectada,
-                        fl.filial as filialAC,
-                        tsc.id_cliente,
-                        concat(na.nombre,' ',na.apellido)as Name,
-                        o.origen_fondos,
-                        tr.transaccion,
-                        tsc.fecha_operacion, 
-                        tsc.monto_transaccion,
-                        f.filial,
-                        cb.colaborador_usuario,
-                        tsc.observaciones
-                    FROM public.transacciones_sin_comprobantes AS tsc
-                    inner join origen_fondos as o on o.id_origen_fondos = tsc.id_origen_fondos
-                    inner join transaccion as tr on tr.id_transaccion = tsc.id_transaccion
-                    inner join filial as f on f.id_filial = tsc.id_filial_realizo_transaccion
-                    inner join filial as fl on fl.id_filial = tsc.id_filial_ac
-                    inner join colaborador as cb on cb.id_colaborador = tsc.id_colaborador
-                    inner join no_afiliado as na on tsc.id_cliente = na.identidad
-
-                    UNION ALL
-
-                    SELECT tsc.id_transaccion_sc, 
-                        tsc.codigo_afiliado, 
-                        tsc.cuenta_afectada,
-                        fl.filial as filialAC,
-                        tsc.id_cliente,
-                        af."OUTAFF_NAME" as Name,
-                        o.origen_fondos,
-                        tr.transaccion,
-                        tsc.fecha_operacion, 
-                        tsc.monto_transaccion,
-                        f.filial,
-                        cb.colaborador_usuario,
-                        tsc.observaciones
-                    FROM public.transacciones_sin_comprobantes AS tsc
-                    inner join origen_fondos as o on o.id_origen_fondos = tsc.id_origen_fondos
-                    inner join transaccion as tr on tr.id_transaccion = tsc.id_transaccion
-                    inner join filial as f on f.id_filial = tsc.id_filial_realizo_transaccion
-                    inner join filial as fl on fl.id_filial = tsc.id_filial_ac
-                    inner join colaborador as cb on cb.id_colaborador = tsc.id_colaborador
-                    inner join public."Afiliados" AS af ON af."OUTAFF_ID" = tsc.id_cliente`, (err, res)=>{
+    conexion.query(`SELECT * FROM public.rte
+                    order by fecha_operacion DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -77,7 +36,8 @@ const getDiligenciaNoAfiliados = (req, resp = response)=>{
                     INNER JOIN razon_operacion AS rp ON rp.id_razon_operacion = DD.id_razon_operacion
                     INNER JOIN transaccion AS ts ON ts.id_transaccion = DD.id_transaccion
                     INNER JOIN colaborador AS cb ON cb.id_colaborador = DD.id_cajero_operacion
-                    INNER JOIN no_afiliado AS na ON na.identidad = DD.id_no_afiliado`, (err, res)=>{
+                    INNER JOIN no_afiliado AS na ON na.identidad = DD.id_no_afiliado
+                    ORDER BY DD.fecha_operacion DESC `, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -88,53 +48,8 @@ const getDiligenciaNoAfiliados = (req, resp = response)=>{
 }
 
 const getChequesTerceros = (req, resp = response)=>{
-    conexion.query(`SELECT ct.id_cheque_tercero, 
-                        ct.codigo_de_afiliado, 
-                        f.filial, 
-                        ct.n_cheque, 
-                        ct.fecha_emision, 
-                        cb.colaborador_nombre,
-                        ct.id_persona,
-                        af."OUTAFF_NAME" AS name,
-                        pt.parentesco,
-                        ct.monto, 
-                        o.origen_fondos,
-                        d.destino,
-                        ct.observaciones, 
-                        ae.afiliado_estado
-                    FROM public.cheques_terceros AS ct
-                    INNER JOIN filial AS f ON f.id_filial = ct.id_filial
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ct.id_colaborador
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ct.id_parentesco
-                    INNER JOIN origen_fondos AS o ON o.id_origen_fondos = ct.id_origen_fondos
-                    INNER JOIN destino AS d ON d.id_destino = ct.id_destino
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ct.id_afililiado_estado
-                    INNER JOIN "Afiliados" AS af ON af."OUTAFF_ID" = ct.id_persona
-                    
-                    UNION ALL
-                    
-                    SELECT ct.id_cheque_tercero, 
-                        ct.codigo_de_afiliado, 
-                        f.filial, 
-                        ct.n_cheque, 
-                        ct.fecha_emision, 
-                        cb.colaborador_nombre,
-                        ct.id_persona,
-                        concat(na.nombre, ' ', na.apellido) AS name,
-                        pt.parentesco,
-                        ct.monto, 
-                        o.origen_fondos,
-                        d.destino,
-                        ct.observaciones, 
-                        ae.afiliado_estado
-                    FROM public.cheques_terceros AS ct
-                    INNER JOIN filial AS f ON f.id_filial = ct.id_filial
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ct.id_colaborador
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ct.id_parentesco
-                    INNER JOIN origen_fondos AS o ON o.id_origen_fondos = ct.id_origen_fondos
-                    INNER JOIN destino AS d ON d.id_destino = ct.id_destino
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ct.id_afililiado_estado
-                    INNER JOIN no_afiliado AS na ON na.identidad = ct.id_persona`, (err, res)=>{
+    conexion.query(`SELECT * FROM public.ct
+                    ORDER BY fecha_emision DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
@@ -145,47 +60,8 @@ const getChequesTerceros = (req, resp = response)=>{
 }
 
 const getFirmasTerceros = (req, resp = response)=>{
-    conexion.query(`SELECT ft.id_firma_autorizada, 
-                        af."OUTAFF_NAME" AS name,
-                        ft.id_cliente, 
-                        ae.afiliado_estado,
-                        pt.parentesco,
-                        frm.firma,
-                        ft.codigo_afiliado, 
-                        ft.cuenta_afiliado, 
-                        f.filial, 
-                        ft.apertura_actualizacion, 
-                        cb.colaborador_nombre,
-                        ft.observaciones
-                    FROM public.firmas_autorizadas_terceros AS ft 
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ft.id_afiliado_estado
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ft.id_parentesco
-                    INNER JOIN firma AS frm ON frm.id_firma = ft.id_firma
-                    INNER JOIN filial AS f ON f.id_filial = ft.id_filial_pertenece
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ft.id_colaborador
-                    INNER JOIN "Afiliados" AS af ON af."OUTAFF_ID" = ft.id_cliente
-                    
-                    UNION ALL
-                    
-                    SELECT ft.id_firma_autorizada, 
-                        concat(na.nombre, ' ', na.apellido) AS name,
-                        ft.id_cliente, 
-                        ae.afiliado_estado,
-                        pt.parentesco,
-                        frm.firma,
-                        ft.codigo_afiliado, 
-                        ft.cuenta_afiliado, 
-                        f.filial, 
-                        ft.apertura_actualizacion, 
-                        cb.colaborador_nombre,
-                        ft.observaciones
-                    FROM public.firmas_autorizadas_terceros AS ft 
-                    INNER JOIN afiliado_estado AS ae ON ae.id_afiliado_estado = ft.id_afiliado_estado
-                    INNER JOIN parentesco AS pt ON pt.id_parentesco = ft.id_parentesco
-                    INNER JOIN firma AS frm ON frm.id_firma = ft.id_firma
-                    INNER JOIN filial AS f ON f.id_filial = ft.id_filial_pertenece
-                    INNER JOIN colaborador AS cb ON cb.id_colaborador = ft.id_colaborador
-                    INNER JOIN no_afiliado AS na ON na.identidad = ft.id_cliente`, (err, res)=>{
+    conexion.query(`SELECT * FROM public.ft
+                    ORDER BY apertura_actualizacion DESC`, (err, res)=>{
         if(err){
             return resp.json({
                 msg: "Ha ocurrido un error, por favor contacte al departamente de Ingenieria en sistemas"
